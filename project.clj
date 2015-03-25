ee77ab832761c27998f337cf1b3d8ab44c57cc54
Jordan Gonzalez <jordan.gonzalez@streamlinx.com>
2015-03-25 13:28:48 -0400

    Adding deps

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
-(defproject compojure "1.0.0-SNAPSHOT"
+(defproject cljrest "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [compojure "1.3.2"]
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-json "0.3.1"]
+                 [ring/ring-defaults "0.1.4"]
+                 ])
